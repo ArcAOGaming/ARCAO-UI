a9a1c2d9c429cc0f198e4978974be0845bf8fa24
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-05-16 13:08:21 -0400

    add links

--- src/links.ts
+++ src/links.ts
@@ -1,31 +1,31 @@
-import { ARCAO } from "@arcaogaming/project-links"
+import { ARCAO, SATOSHISPALACE, RANDAO, RUNEREALM } from "@arcaogaming/project-links"
 
 export const ARCAO_LINKS = {
     X: ARCAO.twitterFollow,
-    GITHUB: "https://github.com/ArcAOGaming",
-    DISCORD_INVITE: "https://discord.com/invite/arc-ao",
-    TELEGRAM_INVITE: "https://t.me/ArcAOGames",
-    YOUTUBE: "https://www.youtube.com/channel/UCZ2BLp3TXFAVs6q5S_34H0A",
-    DOCS: "https://docs-arcao_game.ar.ionode.online"
+    GITHUB: ARCAO.github,
+    DISCORD_INVITE: ARCAO.discord,
+    TELEGRAM_INVITE: ARCAO.telegram,
+    YOUTUBE: ARCAO.youtube,
+    DOCS: ARCAO.docs
 }
 
 export const SATOSHIS_PALACE_LINKS = {
-    WEBSITE: "https://satoshispalace.casino/",
-    X: "https://x.com/SatoshisPalaceX",
+    WEBSITE: SATOSHISPALACE.website,
+    X: SATOSHISPALACE.twitterFollow
 }
 
 export const RANDAO_LINKS = {
-    WEBSITE: "https://randao.net",
-    X: "https://x.com/RandAOToken",
-    GITHUB: "https://github.com/RandAOLabs",
-    YOUTUBE: "https://www.youtube.com/@RandAOToken"
+    WEBSITE: RANDAO.website,
+    X: RANDAO.twitterFollow,
+    GITHUB: RANDAO.github,
+    YOUTUBE: RANDAO.youtube
 }
 
 export const RUNREALM_LINKS = {
-    X: "https://x.com/runerealm_ao",
-    GAME: "https://runerealm_game.ar.ionode.online/#/",
+    X: RUNEREALM.twitterFollow,
+    GAME: RUNEREALM.gameSite,
     // LANDING_PAGE: "",
-    PREMIUM_GAME: "https://premium_game.ar.ionode.online/",
-    GITHUB: "https://github.com/orgs/RuneRealm/repositories",
+    PREMIUM_GAME: RUNEREALM.premiumGameSite,
+    GITHUB: RUNEREALM.github
     // YOUTUBE: ""
 }
